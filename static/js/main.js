fca4c57cfd011ce739f0fe4f8dbe3c47bf2707f1
Orest <orest@orest.me>
2025-03-15 10:13:11 +0300

    autocommit

--- static/js/main.js
+++
@@ -1,7 +0,0 @@
-function setRandomBackground() {
-    document.body.style.backgroundColor = `hsl(${Math.floor(Math.random() * 360)}, 100%, 90%)`;
-}
-
-setRandomBackground();
-
-setInterval(setRandomBackground, 4000);
